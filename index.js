7a19c74048c6e8b2d09ea14f58e6aa3157eb2b87
Natalia Sereda <seredanatasha@gmail.com>
2024-09-02 14:22:28 +0400

    code refactoring

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const { version } = require('./package.json');
 console.log(`Hi, version is ${version}`);
 
 const calcPolishBackNotation = require('./polishBackNotation');
-console.log(calcPolishBackNotation());
+
 
 
